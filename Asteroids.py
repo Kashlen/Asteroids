dd48f11959645fb8aff5ed7a97796cc882984bda
Lenka Kášová <kasova.lenka@email.cz>
2022-02-11 14:17:24 +0100

    Class SpaceObject and Asteroid added
    Class SpaceObject made parent class, classes Spaceship and Asteroid are
    inherited.

--- Asteroids.py
+++ Asteroids.py
@@ -1,3 +1,5 @@
+from random import randint, uniform, choice
+import os
 import pyglet, math
 from pyglet import gl
 from pyglet.window import key
@@ -16,21 +18,29 @@ dt = pyglet.clock.tick()
 window = pyglet.window.Window(width=WIDTH, height=HEIGHT)
 
 
-class Spaceship:
-    def __init__(self, rotation):
-        self.x, self.y = [WIDTH // 2, HEIGHT // 5]
-        self.x_speed = 50
-        self.y_speed = 50
+class SpaceObject:
+    def __init__(self, x, y, rotation, x_speed, y_speed):
+        self.x, self.y = [x, y]
+        self.x_speed = x_speed
+        self.y_speed = y_speed
         self.rotation = rotation
-        image = pyglet.image.load("resources/blue_spaceship.png")
-        image.anchor_x = image.width // 2
-        image.anchor_y = image.height // 2
-        self.sprite = pyglet.sprite.Sprite(image, batch=game_batch)
 
     def tick(self, dt):
         self.x = self.x + dt * self.x_speed * math.cos(self.rotation)
         self.y = self.y + dt * self.y_speed * math.sin(self.rotation)
         self.sprite.x, self.sprite.y = [self.x, self.y]   
+       
+
+class Spaceship(SpaceObject):
+    def __init__(self):
+        super().__init__(x=WIDTH // 2, y=HEIGHT // 5, rotation=1.5708, x_speed=50, y_speed=50)
+        image = pyglet.image.load("resources/blue_spaceship.png")
+        image.anchor_x = image.width // 2
+        image.anchor_y = image.height // 2
+        self.sprite = pyglet.sprite.Sprite(image, batch=game_batch)
+        
+    def tick(self, dt):
+        super().tick(dt)
         if "left" in pressed_keys:
             self.rotation = self.rotation + dt * ROTATION_SPEED
             self.sprite.rotation = 90 - math.degrees(self.rotation)
@@ -40,6 +50,16 @@ class Spaceship:
         if "up" in pressed_keys:
             self.x_speed += dt * ACCELERATION
             self.y_speed += dt * ACCELERATION
+
+class Asteroid(SpaceObject):
+    def __init__(self):
+        super().__init__(x=0, y=0, rotation=uniform(0,6.3), x_speed=randint(20,50), y_speed=randint(20,50))
+        images = os.listdir("resources/asteroid")
+        png = choice(images)
+        image_a = pyglet.image.load("resources/asteroid/" + png)
+        image_a.anchor_x = image_a.width // 2
+        image_a.anchor_y = image_a.height // 2
+        self.sprite = pyglet.sprite.Sprite(image_a, batch=game_batch)
 
 
 def pressed_key(symbol, modifiers):
@@ -72,9 +92,12 @@ def draw():
 
             # Restore remembered state (this cancels the glTranslatef)
             gl.glPopMatrix()
-            
-ship = Spaceship(1.5708)
-ship2 = Spaceship(0.0708)
+
+ship = Spaceship()
+asteroid1 = Asteroid()
+asteroid2 = Asteroid()
+asteroid3 = Asteroid()
+
 
 window.push_handlers(
     on_draw = draw,
@@ -84,5 +107,7 @@ window.push_handlers(
 
 
 pyglet.clock.schedule(ship.tick)
-pyglet.clock.schedule(ship2.tick)
+pyglet.clock.schedule(asteroid1.tick)
+pyglet.clock.schedule(asteroid2.tick)
+pyglet.clock.schedule(asteroid3.tick)
 pyglet.app.run()
